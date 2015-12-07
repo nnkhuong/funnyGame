ddb9c0fdd876b11f3640754715a29e35867f500c
nnkhuong <nguyenngockhuong@gmail.com>
2015-12-06 21:55:13 -0600

    add qr code

---
+++ CloserMe/app.js
@@ -0,0 +1,50 @@
+
+var express = require('express');
+var path = require('path');
+//var bodyParser = require('body-parser');
+var qrImage = require('qr-image');
+var fs = require('fs');
+
+var app = express();
+
+// configure app
+
+app.set('view engine', 'ejs');
+app.set('views' , path.join(__dirname, 'views'));
+app.use(express.static("public"));
+// use middleware
+app.use(bodyParser());
+
+// define routes
+
+var imageQR_File_Name = "";
+var locationQR_File = "public/MyQRCode.png";
+
+app.get('/', function (req, res) {
+  res.render('index', {imageQR : imageQR_File_Name});
+});
+
+app.post('/generateQR', function(req, res) {
+	var rString = randomString(20, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+	imageQR_File_Name = "MyQRCode.png";
+	qrImage.image(rString, {type:'png', size:20}).pipe(fs.createWriteStream(locationQR_File));
+	 
+	res.redirect('/');
+});
+
+
+function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+  var rString = randomString(20, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+  console.log(rString);
+});
---
+++ CloserMe/readme.txt
@@ -0,0 +1,9 @@
+Deploy website
+
+npm init => package.json
+npm install qr-image --save
+rpm install express --save
+rpm install express
+
+start server: node app.js
+browser: localhost:3000
